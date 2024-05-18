ab0ec8bc34af06643217e63b3e7f32c685cc0b39
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2024-05-18 14:32:12 +0200

    Way off in the deep end like usual

--- apps/web/containers/async-page.tsx
+++ apps/web/containers/async-page.tsx
@@ -1,41 +1,79 @@
 "use client";
-import { Faucet } from "@/components/faucet";
-import { useFaucet } from "@/lib/stores/balances";
-import { useWalletStore } from "@/lib/stores/wallet";
-import { Swap } from "@/components/swap";
-import { useBalancesStore } from "@/lib/stores/balances";
+
+import { Home as HomeComponent } from "@/components/home";
+import { Balances, Wallet } from "@/components/wallet/wallet";
+import { useEffect, useMemo, useState } from "react";
+import { useNotifyTransactions, useWalletStore } from "@/lib/stores/wallet";
+import {
+  useBalancesStore,
+  useFaucet,
+  useObserveBalance,
+} from "@/lib/stores/balances";
+import { useChainStore, usePollBlockHeight } from "@/lib/stores/chain";
+import { tokens } from "@/tokens";
 import { useClientStore } from "@/lib/stores/client";
+import { FaucetForm } from "@/components/faucet/faucet-form";
+import { AddLiquidityForm } from "./xyk/add-liquidity-form";
+import { RemoveLiquidityForm } from "./xyk/remove-liquidity-form";
+import { SwapForm } from "./xyk/swap-form";
+import { TransferForm } from "./wallet/transfer-form";
 
 export default function Home() {
-  const wallet = useWalletStore();
-  const drip = useFaucet();
-  const balances = useBalancesStore();
-  const clientStore = useClientStore();
+  const { connectWallet, wallet, observeWalletChange, initializeWallet } =
+    useWalletStore();
+  const client = useClientStore();
 
-  const handleSwap = async (fromToken: string, toToken: string, amount: number) => {
-    if (wallet.wallet && clientStore.client) {
-      await balances.swapTokens(clientStore.client, fromToken, toToken, amount, wallet.wallet);
-    }
-  };
+  const {
+    balances,
+    loading: balancesLoading,
+    clearBalances,
+  } = useBalancesStore();
+  const { block } = useChainStore();
+  usePollBlockHeight();
+
+  useNotifyTransactions();
+
+  useEffect(() => {
+    wallet && clearBalances(wallet);
+  }, [wallet]);
+  Object.keys(tokens).forEach((tokenId) => {
+    useObserveBalance(tokenId, wallet);
+  });
+
+  useEffect(() => {
+    client.start();
+    observeWalletChange();
+    initializeWallet();
+  }, []);
+
+  const ownBalances = wallet ? balances[wallet] : {};
+
+  const loading =
+    balancesLoading && !!(wallet && balances[wallet]?.["0"] === undefined);
+
+  const faucet = useFaucet();
 
   return (
-    <div className="mx-auto -mt-32 h-full pt-16">
-      <div className="flex h-full w-full items-center justify-center pt-16">
-        <div className="flex basis-4/12 flex-col items-center justify-center 2xl:basis-3/12">
-          <Faucet
-            wallet={wallet.wallet}
-            onConnectWallet={wallet.connectWallet}
-            onDrip={drip}
-            loading={false}
+    <>
+      <HomeComponent
+        swapForm={<SwapForm />}
+        transferForm={<TransferForm />}
+        addLiquidityForm={<AddLiquidityForm />}
+        removeLiquidityForm={<RemoveLiquidityForm />}
+        wallet={
+          <Wallet
+            loading={loading}
+            blockHeight={block?.height}
+            address={wallet}
+            balances={ownBalances}
+            onConnectWallet={async () => {
+              await connectWallet();
+            }}
+            forceIsWalletOpen={!!wallet}
+            onFaucetDrip={() => client.client && wallet && faucet()}
           />
-          <Swap
-            wallet={wallet.wallet}
-            onConnectWallet={wallet.connectWallet}
-            onSwap={handleSwap}
-            loading={false}
-          />
-        </div>
-      </div>
-    </div>
+        }
+      />
+    </>
   );
 }
--- apps/web/lib/stores/balances.tsx
+++ apps/web/lib/stores/balances.tsx
@@ -2,24 +2,44 @@ import { create } from "zustand";
 import { Client, useClientStore } from "./client";
 import { immer } from "zustand/middleware/immer";
 import { PendingTransaction, UnsignedTransaction } from "@proto-kit/sequencer";
-import { BalancesKey, TokenId } from "@proto-kit/library";
-import { PublicKey, UInt64 } from "o1js";
-import { useCallback, useEffect } from "react";
+import { Balance, BalancesKey, TokenId } from "@proto-kit/library";
+import { PublicKey } from "o1js";
+import { useCallback, useEffect, useMemo } from "react";
 import { useChainStore } from "./chain";
 import { useWalletStore } from "./wallet";
 
 export interface BalancesState {
   loading: boolean;
   balances: {
-    // address - balance
-    [key: string]: string;
+    // address
+    [key: string]:
+      | {
+          // tokenId
+          [key: string]: string | undefined;
+        }
+      | undefined;
   };
-  loadBalance: (client: Client, address: string) => Promise<void>;
+  totalSupply: {
+    [key: string]: string | undefined;
+  };
+  loadBalance: (
+    client: Client,
+    tokenId: string,
+    address: string,
+  ) => Promise<void>;
+  clearBalances: (address?: string) => void;
   faucet: (client: Client, address: string) => Promise<PendingTransaction>;
-  swapTokens: (client: Client, fromToken: string, toToken: string, amount: number, address: string) => Promise<void>;
+  loadTotalSupply: (client: Client, tokenId: string) => Promise<void>;
+  transfer: (
+    client: Client,
+    sender: string,
+    tokenId: string,
+    address: string,
+    amount: string,
+  ) => Promise<PendingTransaction>;
 }
 
-function isPendingTransaction(
+export function isPendingTransaction(
   transaction: PendingTransaction | UnsignedTransaction | undefined,
 ): asserts transaction is PendingTransaction {
   if (!(transaction instanceof PendingTransaction))
@@ -33,28 +53,64 @@ export const useBalancesStore = create<
   [["zustand/immer", never]]
 >(
   immer((set) => ({
-    loading: false,
+    loading: Boolean(false),
     balances: {},
-    async loadBalance(client: Client, address: string) {
+    totalSupply: {},
+    async loadTotalSupply(client: Client, tokenId: string) {
       set((state) => {
         state.loading = true;
       });
 
-      const key = BalancesKey.from(tokenId, PublicKey.fromBase58(address));
+      const totalSupply = await client.query.runtime.Balances.totalSupply.get(
+        TokenId.from(tokenId),
+      );
+
+      set((state) => {
+        state.loading = false;
+        state.totalSupply = {
+          ...state.totalSupply,
+          [tokenId]: totalSupply?.toString() ?? "0",
+        };
+      });
+    },
+    clearBalances(address) {
+      set((state) => {
+        if (address) {
+          delete state.balances[address];
+        } else {
+          state.balances = {};
+        }
+      });
+    },
+    async loadBalance(client: Client, tokenId: string, address: string) {
+      set((state) => {
+        state.loading = true;
+      });
+
+      const key = BalancesKey.from(
+        TokenId.from(tokenId),
+        PublicKey.fromBase58(address),
+      );
 
       const balance = await client.query.runtime.Balances.balances.get(key);
 
       set((state) => {
         state.loading = false;
-        state.balances[address] = balance?.toString() ?? "0";
+        state.balances = {
+          ...state.balances,
+          [address]: {
+            ...state.balances[address],
+            [tokenId]: balance?.toString() ?? "0",
+          },
+        };
       });
     },
     async faucet(client: Client, address: string) {
-      const balances = client.runtime.resolve("Balances");
+      const faucet = client.runtime.resolve("Faucet");
       const sender = PublicKey.fromBase58(address);
 
       const tx = await client.transaction(sender, () => {
-        balances.addBalance(tokenId, sender, UInt64.from(1000));
+        faucet.dripBundle();
       });
 
       await tx.sign();
@@ -63,25 +119,57 @@ export const useBalancesStore = create<
       isPendingTransaction(tx.transaction);
       return tx.transaction;
     },
-    async swapTokens(client: Client, fromToken: string, toToken: string, amount: number, address: string) {
-      // Simulate a swap operation for now
-      console.log(`Swapping ${amount} from ${fromToken} to ${toToken} for address ${address}`);
-      // Implement the swap logic here
+    async transfer(
+      client: Client,
+      address: string,
+      tokenId: string,
+      recipient: string,
+      amount: string,
+    ) {
+      const balances = client.runtime.resolve("Balances");
+      const sender = PublicKey.fromBase58(address);
+
+      const tx = await client.transaction(sender, () => {
+        balances.transferSigned(
+          TokenId.from(tokenId),
+          sender,
+          PublicKey.fromBase58(recipient),
+          Balance.from(amount),
+        );
+      });
+
+      await tx.sign();
+      await tx.send();
+
+      isPendingTransaction(tx.transaction);
+      return tx.transaction;
     },
-  }))
+  })),
 );
 
-export const useObserveBalance = () => {
-  const client = useClientStore();
-  const chain = useChainStore();
-  const wallet = useWalletStore();
+export const useBalance = (address?: string, tokenId?: string) => {
   const balances = useBalancesStore();
 
-  useEffect(() => {
-    if (!client.client || !wallet.wallet) return;
+  return useMemo(() => {
+    if (!address || !tokenId) return;
 
-    balances.loadBalance(client.client, wallet.wallet);
-  }, [client.client, chain.block?.height, wallet.wallet]);
+    return balances.balances[address]?.[tokenId];
+  }, [balances.balances, address, tokenId]);
+};
+
+export const useObserveBalance = (tokenId?: string, address?: string) => {
+  const client = useClientStore();
+  const chain = useChainStore();
+  const balances = useBalancesStore();
+  const balance = useBalance(address, tokenId);
+
+  useEffect(() => {
+    if (!client.client || !address || !tokenId) return;
+
+    balances.loadBalance(client.client, tokenId, address);
+  }, [client.client, chain.block?.height, address]);
+
+  return balance;
 };
 
 export const useFaucet = () => {
@@ -99,4 +187,51 @@ export const useFaucet = () => {
 
     wallet.addPendingTransaction(pendingTransaction);
   }, [client.client, wallet.wallet]);
+};
+
+export const useTransfer = () => {
+  const client = useClientStore();
+  const balances = useBalancesStore();
+  const wallet = useWalletStore();
+
+  return useCallback(
+    async (tokenId: string, recipient: string, amount: string) => {
+      if (!client.client || !wallet.wallet) return;
+
+      const pendingTransaction = await balances.transfer(
+        client.client,
+        wallet.wallet,
+        tokenId,
+        recipient,
+        amount,
+      );
+
+      wallet.addPendingTransaction(pendingTransaction);
+    },
+    [client.client, wallet.wallet],
+  );
+};
+
+export const useTotalSupply = (tokenId?: string) => {
+  const balances = useBalancesStore();
+
+  return useMemo(() => {
+    if (!tokenId) return "0";
+    return balances.totalSupply[tokenId];
+  }, [tokenId, balances]);
+};
+
+export const useObserveTotalSupply = (tokenId?: string) => {
+  const client = useClientStore();
+  const balances = useBalancesStore();
+  const totalSupply = useTotalSupply(tokenId);
+  const chain = useChainStore();
+
+  useEffect(() => {
+    if (!client.client || !tokenId) return;
+
+    balances.loadTotalSupply(client.client, tokenId);
+  }, [client.client, tokenId, chain.block?.height]);
+
+  return totalSupply;
 };
