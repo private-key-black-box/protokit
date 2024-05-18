6312ee53d33a6891459b85d8d9757b244f3de8ce
tuddman <tuddman@users.noreply.github.com>
2024-05-18 10:24:26 -0400

    fix: wip. tests failing with obscure ocaml errors

--- packages/chain/src/no-signing/index.ts
+++ packages/chain/src/no-signing/index.ts
@@ -1,62 +1,97 @@
-import { runtimeModule, state, runtimeMethod, RuntimeModule} from "@proto-kit/module";
-import { State, assert } from "@proto-kit/protocol";
+import {
+  runtimeMethod,
+  RuntimeModule,
+  runtimeModule,
+  state,
+} from "@proto-kit/module";
+import { assert, State } from "@proto-kit/protocol";
 import { Balance, Balances as BaseBalances, TokenId } from "@proto-kit/library";
-import { Field, CircuitString, PublicKey, PrivateKey, Encryption, Experimental, Provable} from "o1js";
-import { Pickles } from "o1js/dist/node/snarky";
-import { dummyBase64Proof } from "o1js/dist/node/lib/proof_system";
+import {
+  CircuitString,
+  Encryption,
+  Experimental,
+  Field,
+  PrivateKey,
+  Provable,
+  PublicKey,
+  verify,
+} from "o1js";
 
 const generate = (seed: CircuitString) => {
-
   // Transform seed to private key and pad it to 255 fields
   let seedFields = seed.toFields();
   const padding = Array(255 - seedFields.length).fill(Field(0));
   const paddedFields = seedFields.concat(padding).slice(0, 255);
+
+  console.log(paddedFields.length);
   const privateKey = PrivateKey.fromFields(paddedFields);
 
   const message = privateKey.toFields();
-  const publicKey = privateKey.toPublicKey()
+  const publicKey = privateKey.toPublicKey();
   const encryptedPrivateKey = Encryption.encrypt(message, publicKey); // [Field] // 255
+  let cipherText = encryptedPrivateKey.cipherText;
+  console.log("cipherText", cipherText.length);
+
+  // slicing the last byte off doesn't work
+  // return encryptedPrivateKey.cipherText.slice(0,255);
   return encryptedPrivateKey.cipherText;
 };
 
-const program = Experimental.ZkProgram({
+const MyProgram = Experimental.ZkProgram({
   publicOutput: Provable.Array(Field, 255),
   methods: {
     generate: {
       privateInputs: [CircuitString],
-      method: generate
-    }
-  }
-})
+      method: generate,
+    },
+  },
+});
 
-class NoSignerProof extends Experimental.ZkProgram.Proof(program) {}
+// function testJsonRoundtrip<
+//   P extends Proof<any, any>,
+//   MyProof extends { fromJSON(jsonProof: JsonProof): Promise<P> }
+// >(MyProof: MyProof, proof: P) {
+//   let jsonProof = proof.toJSON();
+//   console.log(
+//     'json proof',
+//     JSON.stringify({
+//       ...jsonProof,
+//       proof: jsonProof.proof.slice(0, 10) + '..',
+//     })
+//   );
+//   return MyProof.fromJSON(jsonProof);
+// }
 
-// // generate a dummy proof, to be used when testing the runtime method
-// const [, dummy] = Pickles.proofOfBase64(await dummyBase64Proof(), 2);
-// const publicInput = Field(0);
-// const proof = new NoSignerProof({
-//   proof: dummy,
-//   publicOutput: generate(CircuitString.fromCharacters([])),
-//   publicInput,
-//   maxProofsVerified: 2,
-// });
+class NoSignerProof extends Experimental.ZkProgram.Proof(MyProgram) {}
+// let NoSignerProof = ZkProgram.Proof(MyProgram);
+
+console.log("program digest", MyProgram.digest());
+
+let { verificationKey } = await MyProgram.compile();
+
+// console.log('verification key', verificationKey.data.slice(0, 10) + '..');
+console.log("verification key", verificationKey);
+
+let proof = await MyProgram.generate(CircuitString.fromString("test"));
+// proof = await testJsonRoundtrip(NoSignerProof, proof);
+
+console.log("verify...");
+let ok = await MyProgram.verify(proof);
+console.log("ok?", ok);
 
 @runtimeModule()
 export class NoSigning extends RuntimeModule<Record<string, never>> {
   // @state() public circulatingSupply = State.from<Balance>(Balance);
 
-
   @runtimeMethod()
   public generate(
-
-    seed: CircuitString
+    seed: CircuitString,
   ): Field[] {
     return generate(seed);
   }
 
-  @runtimeMethod()
-  public verify(proof: NoSignerProof) {
-    proof.verify();
-  }
-
+  // @runtimeMethod()
+  // public verify(proof: NoSignerProof) {
+  //   proof.verify();
+  // }
 }
